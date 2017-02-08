72baebd2835b4732ee0507e64b52da4ba422bd50
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-07 23:24:48 -1000

    selection sort working. created a swap fucntion that swaps min values

--- selection-sort.js
+++ selection-sort.js
@@ -2,8 +2,26 @@
 
 let myArray = [9,2,5,6,4,3,7,10,1,8,11];
 
+function swap(myArray, i, j){
+  var tmp = myArray[i];
+  myArray[i] = myArray[j];
+  myArray[j] = tmp;
+}
+
 function selectionSort(myArray) {
-  // body...
+  for (var i = 0; i < myArray.length; i++) {
+    var min = i;
+    for (var j = i + 1; j < myArray.length; j++) {
+      if(myArray[j] < myArray[min]){
+        min = j;
+      }
+    }
+    if(i !== min){
+      swap(myArray, i, min);
+      console.log(myArray);
+    }
+  }
+  return myArray;
 }
 
 console.log(selectionSort(myArray));
